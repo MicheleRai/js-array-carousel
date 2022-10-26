c71c1e1c2a9821912656b81dbd783fec8114f2a4
MicheleRai <rainonemichele1999@gmail.com>
2022-10-26 14:21:57 +0200

    base model

--- js/script.js
+++ js/script.js
@@ -12,3 +12,62 @@
 // BONUS 2:
 // Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato.
 // Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
+
+const arrImages = [
+	'img/01.jpg',
+	'img/02.jpg',
+	'img/03.jpg',
+	'img/04.jpg',
+	'img/05.jpg',
+];
+
+const eleSlider = document.querySelector('.slider');
+const eleBtnUp = document.querySelector('.btn-up');
+const eleBtnDown = document.querySelector('.btn-down');
+
+
+for (let i = 0; i < arrImages.length; i++) {
+	const eleImg = document.createElement('img');
+	eleImg.src = arrImages[i];
+	eleImg.classList.add('slider-img');
+
+	if (i === 0) {
+		eleImg.classList.add('active');
+	}
+
+	eleSlider.append(eleImg);
+}
+// document.querySelector('.slider-img').classList.add('active');
+
+const listEleImg = document.querySelectorAll('.slider-img'); // non e' un array ma qualcosa di simile
+
+let activeIndex = 0;
+
+
+eleBtnDown.addEventListener('click', function () {
+	
+	listEleImg[activeIndex].classList.remove('active');
+
+	activeIndex++;
+
+	listEleImg[activeIndex].classList.add('active');
+
+	eleBtnUp.classList.remove('hidden');
+	if (activeIndex === listEleImg.length - 1) {
+		eleBtnDown.classList.add('hidden');
+	}
+});
+
+eleBtnUp.addEventListener('click', function () {
+	
+	listEleImg[activeIndex].classList.remove('active');
+
+	activeIndex--;
+
+	listEleImg[activeIndex].classList.add('active');
+
+	eleBtnDown.classList.remove('hidden');
+	if (activeIndex === 0) {
+		eleBtnUp.classList.add('hidden');
+	}
+});
